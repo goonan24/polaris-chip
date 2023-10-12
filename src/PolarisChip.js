d4d8148445833194d612d2ad36c471d8c55a32e1
goonan24 <jgoonan24@gmail.com>
2023-10-12 13:03:09 -0400

    commit

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -3,24 +3,44 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: { type: String},
+      active: {Type: Boolean, reflect: true}
     };
   }
 
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-      }
-    `;
-  }
+static get styles() {
+  return css`
+    :host {
+      display: inline-block;
+      margin: 0 16px 16px 0;
+    }
 
-  constructor() {
-    super();
-    this.title = 'My boilerplate';
-  }
+    .link {
+      font-weight: bold;
+      text-decoration: none;
+      padding: 8px 4px;
+      border: 2px solid black;
+      color: black;
+      font-size: 14px;
+    }
 
-  render() {
-    return html`<span>${this.title}</span>`;
-  }
+    :host([active]) .link:hover {
+      background-color: gray;
+      border: 2px solid black;
+      border-radius: 2px;
+      color: #005fa9;
+      cursor: pointer;
+    }
+  `;
+}
+constructor() {
+  super();
+  this.name = '';
+  this.link = "https://www.psu.edu/research/";
+}
+
+render() {
+  return html`<a class="link" href=${this.link}><slot>${this.name}</slot></a>`;
+}
 }
